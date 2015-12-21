2d63b98498a1a8bf82c9c4856034d593ece27512
Richard Bowden <rb@rdbg.co.uk>
2015-12-21 11:38:41 +0000

    removed pi service

--- main.go
+++ main.go
@@ -19,16 +19,16 @@ func main() {
 	var envErrors []string
 
 	rndBackendURL := os.Getenv(rndBackendURLEnv)
-	calcPiBackendURL := os.Getenv(calcPiBackendURLEnv)
+	// calcPiBackendURL := os.Getenv(calcPiBackendURLEnv)
 	servePort := os.Getenv(serverPortEnv)
 
 	if rndBackendURL == "" {
 		envErrors = append(envErrors, fmt.Sprintf("%s", rndBackendURLEnv))
 	}
 
-	if calcPiBackendURL == "" {
-		envErrors = append(envErrors, fmt.Sprintf("%s", calcPiBackendURLEnv))
-	}
+	// if calcPiBackendURL == "" {
+	// 	envErrors = append(envErrors, fmt.Sprintf("%s", calcPiBackendURLEnv))
+	// }
 
 	if servePort == "" {
 		servePort = defaultServerPort
@@ -40,16 +40,16 @@ func main() {
 
 	bindPort := fmt.Sprintf(":%s", servePort)
 	log.Printf("using RND_BACKEND_URL:%s\n", rndBackendURL)
-	log.Printf("using CALC_PI_BACKEND_URL:%s\n", calcPiBackendURL)
+	// log.Printf("using CALC_PI_BACKEND_URL:%s\n", calcPiBackendURL)
 	log.Printf("serving on forntend on :%s\n", bindPort)
 
 	rndHdl := NewRndHandler(rndBackendURL)
-	calcPiHdl := NewCaclPiHandler(calcPiBackendURL)
+	// calcPiHdl := NewCaclPiHandler(calcPiBackendURL)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/rnd", rndHdl.Calc)
-	r.HandleFunc("/pi", calcPiHdl.Calc)
+	// r.HandleFunc("/pi", calcPiHdl.Calc)
 
 	http.Handle("/", r)
 
