36553574c9ab8a1bbaee57f23aaf512ad9b9b2f6
asega olima <asega.olima@sainsburys.co.uk>
2016-01-21 12:06:57 +0000

    resolving the service config via spring-cloud-config-server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,16 @@ header() {
 header "Starting up..."
 
 header "Resolving the configuration..."
-# TODO: Here we should resolve the configuration from the config service
-export RND_BACKEND_URL="http://random-generator"
-export CALC_PI_BACKEND_URL="http://calc-pi"
-export FRONTEND_PORT=8080
+
+# Resolving the configuration from the config service
+TMP=`tempfile`
+curl --silent http://configs:8888/frontend-${ENVIRONMENT}.properties > $TMP
+while read LINE; do
+    VAR=`echo $LINE | cut -d ':' -f 1`
+    VAL=`echo $LINE | cut -d ' ' -f 2`
+    export $VAR=$VAL
+done < $TMP
+rm -fr $TMP
 
 header "Running the application"
 ./poc/frontend-svc
